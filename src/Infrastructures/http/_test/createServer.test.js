b710c882aa2941ccc0be333c57318b003091917b
rizky.pratama <rizky.pratama@sicepat.com>
2025-05-25 20:49:50 +0700

    feat: update greeting message to 'Hello Dicoding 2024!'

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -50,7 +50,7 @@ describe('HTTP server', () => {
             // Assert
             const responseJson = JSON.parse(response.payload);
             expect(response.statusCode).toEqual(200);
-            expect(responseJson.value).toEqual('Hello Dicoding 2023!');
+            expect(responseJson.value).toEqual('Hello Dicoding 2024!');
         });
     });
 });
